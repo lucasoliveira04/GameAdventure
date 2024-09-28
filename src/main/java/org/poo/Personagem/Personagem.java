4edc33b6742d90fff5523f03d4c3a1b9f44ec898
Lucas Oliveira <camposdlucasoli@gmail.com>
2024-09-27 23:32:48 -0300

    Novos metodos do Personagem

--- src/main/java/org/poo/Personagem/Personagem.java
+++ src/main/java/org/poo/Personagem/Personagem.java
@@ -3,11 +3,13 @@ package org.poo.Personagem;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 public class Personagem {
+    private Long id;
     private String nome;
     private int vida = 100;
     private int energia = 100;
@@ -15,7 +17,16 @@ public class Personagem {
     private int velocidade = 5;
     private int itens;
     private List<Integer> qntdAtaques;
-    private double[] coordenadas;
+    private List<Double> coordenadas;
+    private int fase;
+    private List<Itens> itensColetados;
+
+    public Personagem(){
+        this.coordenadas = new ArrayList<>();
+        this.coordenadas.add(0.0);
+        this.coordenadas.add(0.0);
+        this.itensColetados = new ArrayList<>();
+    }
 
     public boolean atacar() {
         if (this.energia > 0) {
@@ -30,11 +41,62 @@ public class Personagem {
         this.forcaAtaque += item.getAtributosCalculados().getForca();
         this.velocidade += item.getAtributosCalculados().getVelocidade();
         this.itens++;
+        this.itensColetados.add(item);
     }
 
     public void mover(Movement movimento) {
-        this.coordenadas[0] += movimento.getX();
-        this.coordenadas[1] += movimento.getY();
+        if (this.energia > 0){
+            this.energia -= 5;
+            movimento.setX(movimento.getX() + 5);
+            movimento.setY(movimento.getY() + 5);
+            this.coordenadas.add(movimento.getX());
+            this.coordenadas.add(movimento.getY());
+        } else {
+            System.out.println("Sem energia para se mover");
+        }
+    }
+
+    public void mochila(){
+        System.out.println("Mochila");
+        System.out.println("Vida: " + this.vida);
+        System.out.println("Energia: " + this.energia);
+        System.out.println("Itens: " + this.itens);
+        listarPorRaridade();
+    }
+
+    public void listarPorRaridade(){
+        List<Itens> itensComum = new ArrayList<>();
+        List<Itens> itensRaro = new ArrayList<>();
+        List<Itens> itensLendario = new ArrayList<>();
+
+        for (Itens item : itensColetados){
+            switch (item.getRaridade()){
+                case "comum":
+                    itensComum.add(item);
+                    break;
+                case "raro":
+                    itensRaro.add(item);
+                    break;
+                case "lendario":
+                    itensLendario.add(item);
+                    break;
+            }
+        }
+
+        System.out.println("Itens comuns: ");
+        for (Itens item : itensComum){
+            System.out.println(item.getNome());
+        }
+
+        System.out.println("Itens raros: ");
+        for (Itens item : itensRaro){
+            System.out.println(item.getNome());
+        }
+
+        System.out.println("Itens lendários: ");
+        for (Itens item : itensLendario){
+            System.out.println(item.getNome());
+        }
     }
 
 }
